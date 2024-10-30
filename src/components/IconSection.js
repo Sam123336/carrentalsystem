d0b47f6d8847a552685f7ac2178078b0cca28700
Sam Coder <115978232+Sam123336@users.noreply.github.com>
2024-10-30 13:21:45 +0530

    commit it

--- src/components/IconSection.js
+++ src/components/IconSection.js
@@ -1,14 +1,63 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { motion, useScroll } from 'framer-motion';
+
+import BMW from "./images/BMW.png";
+import Cadillac from "./images/cadalic.png";
+import Lamborghini from "./images/Lamborghini.png";
+import jeep from "./images/jeep.png";
+import ford from "./images/ford.png";
+import nisaan from "./images/nisaan.png";
+
 function IconSection() {
-    return (
-      <section className="flex justify-around py-6 bg-gray-800">
-        {/* Replace with appropriate icons */}
-        <div className="text-gray-400">Brand 1</div>
-        <div className="text-gray-400">Brand 2</div>
-        <div className="text-gray-400">Brand 3</div>
-        <div className="text-gray-400">Brand 4</div>
-        <div className="text-gray-400">Brand 5</div>
-      </section>
-    );
-  }
-  export default IconSection;
+  const brands = [
+   
+    { name: 'Brand 2', image: Cadillac },
+    { name: 'Brand 3', image: Lamborghini },
+    { name: 'Brand 2', image: Cadillac },
+    { name: 'Brand 2', image: Cadillac },
+    { name: 'Brand 2', image: Cadillac },
+    { name: 'Brand 2', image: Cadillac },
+    { name: 'Brand 2', image: Cadillac },
+    { name: 'Brand 2', image: Cadillac },
+    { name: 'Brand 2', image: Cadillac },
+    { name: 'Brand 2', image: Cadillac },
+    { name: 'Brand 5', image: ford },
+    
+  ];
+
+  const ref = useRef(null);
+  const { scrollXProgress } = useScroll({ container: ref });
+
+  return (
+    <section className="py-6 bg-gray-800 overflow-x-hidden">
+      <ul
+        ref={ref}
+        className="flex space-x-10 scrollbar-hide px-4" // Increased spacing between items
+        style={{
+          whiteSpace: 'nowrap',
+          overflowY: 'hidden',
+          paddingBottom: '10px',
+          margin: '0',
+          height: '400px', // Adjusted for larger component height
+        }}
+      >
+        {brands.map((brand, index) => (
+          <motion.li
+            key={index}
+            className="flex-shrink-0 text-center text-gray-400 w-64" // Adjusted width for larger items
+            whileHover={{ scale: 1.1 }}
+          >
+            <img
+              src={brand.image}
+              alt={`${brand.name} car`}
+              className="w-80 h-64 mx-auto mb-4" // Adjusted image size
+            />
+            <div className="text-lg font-semibold">{brand.name}</div> {/* Slightly larger font for brand name */}
+          </motion.li>
+        ))}
+      </ul>
+    </section>
+  );
+}
+
+export default IconSection;
